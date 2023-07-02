04a3880413f6ca310ccd5f5974bae0508bc2ab82
georgemakrakis <georgemakrakis88@gmail.com>
2023-07-02 14:50:28 -0600

    Created .sh scripts for GOOSE testing

---
+++ goose_publisher_test.sh
@@ -0,0 +1,2 @@
+cd /home/mininet/libiec61850/examples/goose_publisher
+make && ./goose_publisher_example $1
---
+++ goose_subcriber_test.sh
@@ -0,0 +1,2 @@
+cd /home/mininet/libiec61850/examples/goose_subscriber
+make && ./goose_subscriber_example $1
