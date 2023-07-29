a1e88d4679a5267146ad08ac4c1bbc6af290d9fa
georgemakrakis <georgemakrakis88@gmail.com>
2023-07-29 12:28:55 -0600

    Tried testing both sub and pub in one script
    Added sudo gdb script

--- goose_CHE_203_generic/goose_CHE_203_generic.c
+++ goose_CHE_203_generic/goose_CHE_203_generic.c
@@ -89,22 +89,21 @@ int
 main(int argc, char **argv)
 {
     GooseReceiver receiver = GooseReceiver_create();
+    GoosePublisher publisher;
+    char *interface;
     char *device_name;
 
     if (argc > 1) {
-        printf("Set interface id: %s\n", argv[1]);
-        GooseReceiver_setInterfaceId(receiver, argv[1]);
+        interface = argv[1];
         device_name = argv[2];
+        printf("Set interface id: %s\n",  interface);
+        
+        GooseReceiver_setInterfaceId(receiver, interface);
     }
     else {
         // printf("Using interface eth0\n");
-        printf("Using interface lo\n");
-        // printf("Using interface 487E-eth0\n");
-        // GooseReceiver_setInterfaceId(receiver, "eth0");
-
-        // the interface of SEL 487E device in mininet 
-        // GooseReceiver_setInterfaceId(receiver, "487E-eth0");
-        GooseReceiver_setInterfaceId(receiver, "lo");
+        printf("Not enough parameters, exiting...\n");
+        return 1;
     }
 
     char * devices [] = { "RTAC", "651R_2", "787_2", "451_2" };
@@ -121,5 +120,240 @@ main(int argc, char **argv)
              return 1;
         }
     }
+
+    // First let's setup the subscriber
+    GooseSubscriber subscriber;
+
+    if (strcmp(device_name, "RTAC") == 0) 
+    {
+        printf("GOOSE subscriber RTAC configuration initiated...\n");
+        
+        // This should be sub for data from 651R_2
+        // TODO: We need for the rest of them as well.
+        subscriber = GooseSubscriber_create("simple_651R_2/PRO$CO$BCACSWI2", NULL);
+        uint8_t dstMac[6] = {0x01,0x0c,0xcd,0x01,0x00,0x02};
+        GooseSubscriber_setDstMac(subscriber, dstMac);
+        GooseSubscriber_setAppId(subscriber, 1002);
+    } 
+    else if (strcmp(device_name, "651R_2") == 0)
+    {
+        printf("GOOSE subscriber 651R_2 configuration initiated...\n");
+
+        // This should be sub for data from RTAC
+        // TODO: We need for the rest of them as well.
+        uint8_t dstMac[6] = {0x01,0x0c,0xcd,0x01,0x00,0x01};
+        GooseSubscriber_setDstMac(subscriber, dstMac);
+        GooseSubscriber_setAppId(subscriber, 1001);
+    }
+    else if (strcmp(device_name, "787_2") == 0)
+    {
+        printf("GOOSE subscriber 787_2 configuration initiated...\n");
+    }
+    else if (strcmp(device_name, "451_2") == 0)
+    {
+        printf("GOOSE 451_2 configuration initiated...\n");
+    }
+    else
+    {
+        printf("No device supported, exiting...\n");
+        return 1;
+    }
+
+    //.. and now the publisher
+    LinkedList dataSetValues = LinkedList_create();
+    CommParameters gooseCommParameters;
+    
+    if (strcmp(device_name, "RTAC") == 0) 
+    {
+        printf("GOOSE publisher RTAC configuration initiated...\n");
+        // TODO: 
+        // Should have the integer for the True/False or Open/Close
+        // and another interger for Trip/NoTrip
+        // gooseCommParameters should be here as well.
+        // There should be multiple ones based on the comm paths of
+        // the devices
+
+        // Breaker status (for device No ?) 0/1 or Open/Close.
+        LinkedList_add(dataSetValues, MmsValue_newIntegerFromInt32(0));
+        // LinkedList_add(dataSetValues, MmsValue_newBoolean(true));
+
+        // Trip command (for device No ?) Trip/NoTrip.
+        // LinkedList_add(dataSetValues, MmsValue_newIntegerFromInt32(0));
+        // NOTE: Disable for now might be enabled later.
+        // LinkedList_add(dataSetValues, MmsValue_newBoolean(false));
+
+        gooseCommParameters.appId = 1001;
+        gooseCommParameters.dstAddress[0] = 0x01;
+        gooseCommParameters.dstAddress[1] = 0x0c;
+        gooseCommParameters.dstAddress[2] = 0xcd;
+        gooseCommParameters.dstAddress[3] = 0x01;
+        gooseCommParameters.dstAddress[4] = 0x00;
+        gooseCommParameters.dstAddress[5] = 0x01;
+        gooseCommParameters.vlanId = 0;
+        gooseCommParameters.vlanPriority = 4;
+
+    } 
+    // NOTE: JUST FOR TESTING
+    // else if (strcmp(device_name, "651R_2") == 0)
+    if (strcmp(device_name, "RTAC") == 0)
+    {
+        printf("GOOSE publisher 651R_2 configuration initiated...\n");
+
+        // Breaker status (for device No ?) 0/1 or Open/Close.
+        LinkedList_add(dataSetValues, MmsValue_newIntegerFromInt32(0));
+        // LinkedList_add(dataSetValues, MmsValue_newBoolean(true));
+
+        // Trip command (for device No ?) Trip/NoTrip.
+        // LinkedList_add(dataSetValues, MmsValue_newIntegerFromInt32(0));
+         // NOTE: Disable for now might be enabled later.
+        // LinkedList_add(dataSetValues, MmsValue_newBoolean(false));
+
+        gooseCommParameters.appId = 1002;
+        gooseCommParameters.dstAddress[0] = 0x01;
+        gooseCommParameters.dstAddress[1] = 0x0c;
+        gooseCommParameters.dstAddress[2] = 0xcd;
+        gooseCommParameters.dstAddress[3] = 0x01;
+        gooseCommParameters.dstAddress[4] = 0x00;
+        gooseCommParameters.dstAddress[5] = 0x02;
+        gooseCommParameters.vlanId = 0;
+        gooseCommParameters.vlanPriority = 4;
+
+    }
+    else if (strcmp(device_name, "787_2") == 0)
+    {
+        printf("GOOSE publisher 787_2 configuration initiated...\n");
+    }
+    else if (strcmp(device_name, "451_2") == 0)
+    {
+        printf("GOOSE publisher 451_2 configuration initiated...\n");
+    }
+    else
+    {
+        printf("No device supported, exiting...\n");
+        return 1;
+    }
+
+    publisher = GoosePublisher_create(&gooseCommParameters, interface);
+    // publisher = GoosePublisher_create(&gooseCommParameters, "eth0");
+
+    int i = 0;
+    uint32_t prev_stNum = 0; 
+
+    // TODO: The below should be adjusted for each device
+    // based on the collected information.
+
+    // Interval in ms
+    // int min_interval = 10;
+    int min_interval = 4;
+    // int min_interval = 500;
+    int publish_interval = min_interval;
+    // int max_interval = 100;
+    int max_interval = 1000;
+    // int max_interval = 5000;
+    
+    // NOTE: Used as a simple condition to increase the StNum
+    int max_i = 10;
+    if (publisher) {
+        // TODO: Change the goCbRef and DataSetRef as well
+        // GoosePublisher_setGoCbRef(publisher, "simpleIOGenericIO/LLN0$GO$gcbAnalogValues");
+        GoosePublisher_setConfRev(publisher, 1);
+        // GoosePublisher_setDataSetRef(publisher, "simpleIOGenericIO/LLN0$AnalogValues");
+        GoosePublisher_setTimeAllowedToLive(publisher, 500);
+
+        if (strcmp(device_name, "RTAC") == 0) 
+        {
+            printf("RTAC GOOSE configuration initiated...\n");
+        } 
+        // NOTE: JUST FOR TESTING
+        // else if (strcmp(device_name, "651R_2") == 0)
+        if (strcmp(device_name, "RTAC") == 0)
+        {
+            printf("651R_2 GOOSE configuration initiated...\n");
+
+            // GoosePublisher_setGoCbRef(publisher, "simple_651R_2/LLN0$CO$BCACSWI2$Pos$ctlVal");
+            GoosePublisher_setGoCbRef(publisher, "simple_651R_2/PRO$CO$BCACSWI2");
+            GoosePublisher_setDataSetRef(publisher, "simple_651R_2/PRO$BCACSWI2_DataSet");
+        }
+        else if (strcmp(device_name, "787_2") == 0)
+        {
+            printf("787_2 GOOSE configuration initiated...\n");
+        }
+        else if (strcmp(device_name, "451_2") == 0)
+        {
+            printf("451_2 GOOSE configuration initiated...\n");
+        }
+        else
+        {
+            printf("No device supported, exiting...\n");
+            return 1;
+        }
+    }
+
+
+
+    GooseSubscriber_setListener(subscriber, gooseListener, NULL);
+
+    GooseReceiver_addSubscriber(receiver, subscriber);
+
+    GooseReceiver_start(receiver);
+
+    if (GooseReceiver_isRunning(receiver)) {
+        // ready to receive messages.
+        signal(SIGINT, sigint_handler);
+
+        while (running) {
+            // NOTE: this was for μs
+            // Thread_sleep(100);
+            
+            Thread_sleep(100);
+
+            // Now we also publish based  on the defined interval
+            if (GoosePublisher_publish(publisher, dataSetValues) == -1) {
+                    printf("Error sending message!\n");
+            }
+            Thread_sleep(publish_interval);
+            printf("Publishing...\n");
+
+            // First option 
+            // if( i <= 3) {
+            //     interval = 2 * interval;
+            // }
+            // else {
+            //     interval = max_interval;
+            // }
+
+            // Second option
+            if( i == 0) {
+                publish_interval = min_interval;
+            }
+            else if( i < 3) {
+                publish_interval = 2 * publish_interval;
+            }
+            else {
+                publish_interval = max_interval;
+            }
+            
+            // .. then us a condition to change something to start from the
+            // beginning
+
+            if( i == max_i) {
+                GoosePublisher_increaseStNum(publisher);
+                i = 0;
+                publish_interval = min_interval;
+            }
+
+            i++;
+
+        }
+    }
+    else {
+        printf("Failed to start GOOSE subscriber. Reason can be that the Ethernet interface doesn't exist or root permission are required.\n");
+    }
+
+    GoosePublisher_destroy(publisher);
+
+    GooseReceiver_stop(receiver);
+
+    GooseReceiver_destroy(receiver);
 
 }
