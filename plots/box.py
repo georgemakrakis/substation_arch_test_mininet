20197c785a390541aa5c1723329de40f9b6ea741
georgemakrakis <georgemakrakis88@gmail.com>
2023-09-01 09:10:56 -0600

    Generalizing Python programs for plots

--- plots/box.py
+++ plots/box.py
@@ -11,62 +11,55 @@ multicast_addresses_scenario_1 = [
     "...:04"
 ]
 
-for i in range(1, 5):
-    data = pd.read_csv("../analysis/security_latencies_0{0}.csv".format(i))
-    df = pd.DataFrame(data=data)
+
+
+def main (security):
+    if security:
+        prefix = "../1000/security_latencies_0"
+        title = "With Security"
+        now = datetime.now()
+        final_name = f"box_sec_{now.strftime('%m_%d_%Y-%H_%M_%S')}.png"
+        color = "lightblue"
+    else:
+        prefix = "../1000/learning_latencies_0"
+        title = "Without Security"
+        now = datetime.now()
+        final_name = f"box_no_sec_{now.strftime('%m_%d_%Y-%H_%M_%S')}.png"
+        color = "green"
     
-    sec = [i * 10000 for i in list(df.iloc[:, 1])]
-
-    data_plt.append(sec)
-
-
-fig, ax = plt.subplots()
-plt.xlabel("Multicast Addresses")
-plt.ylabel("Seconds")
-plt.figure(figsize =(100, 100))
-#ax.boxplot(data_dict.values()) # With outliers
-ax.boxplot(data_plt,
-           patch_artist = True,
-           boxprops = dict(facecolor = "lightblue"),
-           showfliers=False)
-ax.set_xticklabels(multicast_addresses_scenario_1)
-# ax.set_xticks(len(multicast_addresses_scenario_1[0]*len(multicast_addresses_scenario_1)))
-
-# Creating plot
-# plt.boxplot(final_data)
-
-now = datetime.now()
-# fig.savefig(f"box_{now.strftime('%m_%d_%Y-%H:%M:%S')}.png")
-
-fig.savefig(f"test_box_sec.png")
+    for i in range(1, 5):
+        data = pd.read_csv("{0}{1}.csv".format(prefix,i))
+        df = pd.DataFrame(data=data)
+        print(df["01:0c:cd:01:00:0{0}".format(i)].max())
+        sec = [i * 10000 for i in list(df.iloc[:, 1])]
 
 
-data_plt = []
-for i in range(1, 5):
-    data = pd.read_csv("../analysis/learning_latencies_0{0}.csv".format(i))
-    df = pd.DataFrame(data=data)
+        data_plt.append(sec)
+
+
+    fig, ax = plt.subplots()
+    plt.xlabel("Multicast Addresses", fontweight="bold")
+    plt.ylabel("ms", fontweight="bold")
+    plt.figure(figsize =(100, 100))
+    #ax.boxplot(data_dict.values()) # With outliers
+    ax.boxplot(data_plt,
+            patch_artist = True,
+            boxprops = dict(facecolor = color),
+            showfliers=False)
+    ax.set_xticklabels(multicast_addresses_scenario_1)
+    # ax.set_xticks(len(multicast_addresses_scenario_1[0]*len(multicast_addresses_scenario_1)))
+
+    # Creating plot
+    # plt.boxplot(final_data)
+
     
-    sec = [i * 10000 for i in list(df.iloc[:, 1])]
 
-    data_plt.append(sec)
+    fig.savefig(final_name)
 
 
-fig, ax = plt.subplots()
-plt.xlabel("Multicast Addresses")
-plt.ylabel("Seconds")
-plt.figure(figsize =(100, 100))
-#ax.boxplot(data_dict.values()) # With outliers
-ax.boxplot(data_plt,
-           patch_artist = True,
-           boxprops = dict(facecolor = "green"),
-           showfliers=False)
-ax.set_xticklabels(multicast_addresses_scenario_1)
-# ax.set_xticks(len(multicast_addresses_scenario_1[0]*len(multicast_addresses_scenario_1)))
+if __name__ == '__main__':
 
-# Creating plot
-# plt.boxplot(final_data)
-
-now = datetime.now()
-# fig.savefig(f"box_{now.strftime('%m_%d_%Y-%H:%M:%S')}.png")
-
-fig.savefig(f"test_box_no_sec.png")
+    security = True
+    # security = False
+    
+    main(security)
--- plots/histograms.py
+++ plots/histograms.py
@@ -4,88 +4,112 @@ import numpy as np
 import pandas as pd
 import statistics
 
-data_plt = []
 
-colors = ["red", "green", "blue", "gray"]
- # # Creating histogram
-fig, axs = plt.subplots(1, 1,
-                    figsize =(10, 7),)
+def main (security):
 
-for i in range(1, 5):
-# for i in range(1, 2):
-    data = pd.read_csv("../analysis/100_security_latencies_0{0}.csv".format(i))
-    df = pd.DataFrame(data=data)
-    
-    # sec = list(df.iloc[:, 1])
-    sec = [i * 10000 for i in list(df.iloc[:, 1])]
-    max_n = max(sec)
-    min_n = min(sec)
+    if security:
+        prefix = "../1000/security_latencies_0"
+        title = "With Security"
+        now = datetime.now()
+        final_name = f"hist_sec_{now.strftime('%m_%d_%Y-%H_%M_%S')}.png"
+    else:
+        prefix = "../1000/learning_latencies_0"
+        title = "Without Security"
+        now = datetime.now()
+        final_name = f"hist_no_sec_{now.strftime('%m_%d_%Y-%H_%M_%S')}.png"
 
-    # print(sec)
 
-    # print(max_n)
-    # print(min_n)
+    data_plt = []
 
-    plt.xlabel("Latency")
-    plt.ylabel("Frequency")
-    
-    
-    plt.hist(sec, bins=20, range=[min_n, 0.7], edgecolor="black", alpha=0.5, facecolor=colors[i-1], align="mid", label="01:0c:cd:01:00:0{0}".format(i))
-    
-    # axs.hist(sec, bins=20, range=[min_n, 0.7], edgecolor="black", facecolor=colors[i-1], align="mid", label="01:0c:cd:01:00:0{0}".format(i))
-    # axs.set_title("01:0c:cd:01:00:0{0}".format(i))
+    colors = ["red", "green", "blue", "gray"]
+    # # Creating histogram
+    fig, axs = plt.subplots(1, 1,
+                        figsize =(10, 7),)
+
+    for i in range(1, 5):
+    # for i in range(1, 2):
+        data = pd.read_csv("{0}{1}.csv".format(prefix,i))
+        df = pd.DataFrame(data=data)
+        
+        # sec = list(df.iloc[:, 1])
+        sec = [i * 10000 for i in list(df.iloc[:, 1])]
+        max_n = max(sec)
+        min_n = min(sec)
+
+        # print(sec)
+
+        # print(max_n)
+        # print(min_n)
+
+        plt.xlabel("Latency")
+        plt.ylabel("Frequency")
+        
+        
+        plt.hist(sec, bins=20, range=[min_n, 0.7], edgecolor="black", alpha=0.5, facecolor=colors[i-1], align="mid", label="01:0c:cd:01:00:0{0}".format(i))
+        
+        # axs.hist(sec, bins=20, range=[min_n, 0.7], edgecolor="black", facecolor=colors[i-1], align="mid", label="01:0c:cd:01:00:0{0}".format(i))
+        # axs.set_title("01:0c:cd:01:00:0{0}".format(i))
+        # fig.savefig("test_hist_sec_{0}.png".format(i))
+
+        # print(df["01:0c:cd:01:00:0{0}".format(i)].describe())
+        # max = df["01:0c:cd:01:00:0{0}".format(i)].max()
+        # min = df["01:0c:cd:01:00:0{0}".format(i)].min()
+
+        # df.plot.hist(bins=10, column=["01:0c:cd:01:00:0{0}".format(i)], range=[min, max])
+        # plt.savefig("test_hist_sec_{0}.png".format(i))
+
+    plt.legend(loc="upper right")
+    now = datetime.now()
+    axs.set_title(title)
     # fig.savefig("test_hist_sec_{0}.png".format(i))
-
-    # print(df["01:0c:cd:01:00:0{0}".format(i)].describe())
-    # max = df["01:0c:cd:01:00:0{0}".format(i)].max()
-    # min = df["01:0c:cd:01:00:0{0}".format(i)].min()
-
-    # df.plot.hist(bins=10, column=["01:0c:cd:01:00:0{0}".format(i)], range=[min, max])
-    # plt.savefig("test_hist_sec_{0}.png".format(i))
-
-plt.legend(loc="upper right")
-now = datetime.now()
-axs.set_title("With Security")
-# fig.savefig("test_hist_sec_{0}.png".format(i))
-fig.savefig(f"hist_sec_{now.strftime('%m_%d_%Y-%H_%M_%S')}.png")
+    fig.savefig(final_name)
+    return
 
 
-fig, axs = plt.subplots(1, 1,
-                figsize =(10, 7),)
-for i in range(1, 5):
-# for i in range(1, 2):
-    data = pd.read_csv("../analysis/100_learning_latencies_0{0}.csv".format(i))    
-    df = pd.DataFrame(data=data)
+# fig, axs = plt.subplots(1, 1,
+#                 figsize =(10, 7),)
+# for i in range(1, 5):
+# # for i in range(1, 2):
+#     data = pd.read_csv("../analysis/100_learning_latencies_0{0}.csv".format(i))    
+#     df = pd.DataFrame(data=data)
     
-    # sec = list(df.iloc[:, 1])
-    sec = [i * 10000 for i in list(df.iloc[:, 1])]
-    max_n = max(sec)
-    min_n = min(sec)
+#     # sec = list(df.iloc[:, 1])
+#     sec = [i * 10000 for i in list(df.iloc[:, 1])]
+#     max_n = max(sec)
+#     min_n = min(sec)
 
-    # print(sec)
+#     # print(sec)
 
-    # print(max_n)
-    # print(min_n)
+#     # print(max_n)
+#     # print(min_n)
 
-    plt.xlabel("Latency")
-    plt.ylabel("Frequency")
+#     plt.xlabel("Latency")
+#     plt.ylabel("Frequency")
     
     
-    plt.hist(sec, bins=20, range=[min_n, 0.7], edgecolor="black", alpha=0.5, facecolor=colors[i-1], align="mid", label="01:0c:cd:01:00:0{0}".format(i))
+#     plt.hist(sec, bins=20, range=[min_n, 0.7], edgecolor="black", alpha=0.5, facecolor=colors[i-1], align="mid", label="01:0c:cd:01:00:0{0}".format(i))
     
-    # axs.hist(sec, bins=20, range=[min_n, 0.7], edgecolor="black", facecolor=colors[i-1], align="mid", label="01:0c:cd:01:00:0{0}".format(i))
-    # axs.set_title("01:0c:cd:01:00:0{0}".format(i))
-    # fig.savefig("test_hist_sec_{0}.png".format(i))
+#     # axs.hist(sec, bins=20, range=[min_n, 0.7], edgecolor="black", facecolor=colors[i-1], align="mid", label="01:0c:cd:01:00:0{0}".format(i))
+#     # axs.set_title("01:0c:cd:01:00:0{0}".format(i))
+#     # fig.savefig("test_hist_sec_{0}.png".format(i))
 
-    # print(df["01:0c:cd:01:00:0{0}".format(i)].describe())
-    # max = df["01:0c:cd:01:00:0{0}".format(i)].max()
-    # min = df["01:0c:cd:01:00:0{0}".format(i)].min()
+#     # print(df["01:0c:cd:01:00:0{0}".format(i)].describe())
+#     # max = df["01:0c:cd:01:00:0{0}".format(i)].max()
+#     # min = df["01:0c:cd:01:00:0{0}".format(i)].min()
 
-    # df.plot.hist(bins=10, column=["01:0c:cd:01:00:0{0}".format(i)], range=[min, max])
-    # plt.savefig("test_hist_sec_{0}.png".format(i))
+#     # df.plot.hist(bins=10, column=["01:0c:cd:01:00:0{0}".format(i)], range=[min, max])
+#     # plt.savefig("test_hist_sec_{0}.png".format(i))
 
-plt.legend(loc="upper right")
-now = datetime.now()
-axs.set_title("Without Security")
-# fig.savefig("test_hist_sec_{0}.png".format(i))
-fig.savefig(f"hist_no_sec_{now.strftime('%m_%d_%Y-%H_%M_%S')}.png")
+# plt.legend(loc="upper right")
+# now = datetime.now()
+# axs.set_title("Without Security")
+# # fig.savefig("test_hist_sec_{0}.png".format(i))
+# fig.savefig(f"hist_no_sec_{now.strftime('%m_%d_%Y-%H_%M_%S')}.png")
+
+
+if __name__ == '__main__':
+
+    security = True
+    # security = False
+    
+    main(security)
